ad2d8217e3a4ac69782860c676ba7f404d2496ce
Keith <kayzee@live.com>
2019-06-17 21:08:49 -1000

    working on 4

--- js/app.js
+++ js/app.js
@@ -1,16 +1,57 @@
 // 1. Add an event to the 'See Details' content that will create an alert box that says 'Not Available in Hawaii.' after clicking on See Details.
 
+let details = document.querySelector("#details");
+
+details.addEventListener("click", function() {
+    alert("Not Available in Hawaii");
+});
+
 //2. Add an event to the div element with the id of 'name1' that will show/hide the description ('descrip1') after hovering over Air Jordan II.
+
+let name1 = document.querySelector("#name1");
+let descrip1 = document.querySelector("#descrip1")
+
+name1.addEventListener("mouseover", function() {
+    // console.log(descrip1);
+    if(descrip1.style.display == 'block'){
+        descrip1.style.display = 'none'
+    }else{
+        descrip1.style.display = 'block'
+    }
+})
 
 //3. Add an event to the div element with the id of 'name2' that will show/hide the following description after clicking on the element.
 //'The instantly recognizable Jumpman silhouette made its debut with the Air Jordan 3 during Michael Jordan's 1987-88 NBA season.'
-
-
 //You'll need to:
 // create a div element with an id of 'descrip2'
 //append this element to the div element with the id of 'name2' 
 
+let name2 = document.querySelector("#name2");
+let descrip2 = document.createElement("div");
+
+descrip2.id = "descrip2";
+descrip2.textContent = "The instantly recognizable Jumpman silhouette made its debut with the Air Jordan 3 during Michael Jordan's 1987-88 NBA season.";
+
+name2.addEventListener("click", function() {
+    // console.log(descrip2);
+    name2.appendChild(descrip2);
+    if(descrip2.style.display == 'block'){
+        descrip2.style.display = 'none'
+    }else{
+        descrip2.style.display = 'block'
+    }
+});
+
+
 //4. Add events to the all the thumbs up icon that will add a count for each time the icon is clicked on for any shoe.
+
+// let thumbsUp = document.querySelectorAll(".fa-thumbs-up");
+
+// thumbsUp.addEventListener("click", function() {
+//     console.log("clicked");
+
+// });
+
 
 //5. Add an event to the plus icon that will increment the price of the Air Jordan V shoe each time the icon is clicked on.
 
