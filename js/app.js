fc0cba24e009ca4f6d37f423d3a71a5ee918c657
Keith <kayzee@live.com>
2019-06-19 14:16:08 -1000

    #6

--- js/app.js
+++ js/app.js
@@ -45,17 +45,45 @@ name2.addEventListener("click", function() {
 
 //4. Add events to the all the thumbs up icon that will add a count for each time the icon is clicked on for any shoe.
 
-// let thumbsUp = document.querySelectorAll(".fa-thumbs-up");
+let thumbsEl = document.querySelectorAll("#thumbs");
 
-// thumbsUp.addEventListener("click", function() {
-//     console.log("clicked");
 
-// });
+for(let i = 0; i < thumbsEl.length; i++) {
+    thumbsEl[i].addEventListener("click", function() {
+        let counter = this.querySelectorAll(".count")[0];
+        counter.innerHTML++;
+    })
+    let counterEl = document.createElement("div");
+    counterEl.className = "count";
+    thumbsEl[i].appendChild(counterEl);
+}
 
 
 //5. Add an event to the plus icon that will increment the price of the Air Jordan V shoe each time the icon is clicked on.
 
+let incPrice = document.querySelector("#increase");
+let price4 = document.querySelector("#price4");
+let price4Str = document.querySelector("#price4").textContent;
+let price4Num = Number(price4Str);
+
+incPrice.addEventListener("click", function() {
+    price4Num += 100;
+    price4.textContent = price4Num;
+    price4.appendChild(incPrice);
+});
+
 //6. Add an event to the minus icon that will decrement the price of the Air Jordan VI shoe each time the icon is clicked on.
+
+let decPrice = document.querySelector("#decrease");
+let price5 = document.querySelector("#price5");
+let price5Str = document.querySelector("#price5").textContent;
+let price5Num = Number(price5Str);
+
+decPrice.addEventListener("click", function() {
+    price5Num -= 100;
+    price5.textContent = price5Num;
+    price5.appendChild(decPrice);
+});
 
 //7. Add an event to the Air Jordan XI shoe that will show another colorway for that shoe after hovering over the image. 
 
@@ -64,3 +92,4 @@ name2.addEventListener("click", function() {
 //9. Add events to both the plus and minus icon that will add or decrease the price each time the respective icon is clicked on.
 
 //10. Add an event to the Air Jordan XXVIII shoe that will show the famous 'crying Michael Jordan' meme after clicking on the image.
+
